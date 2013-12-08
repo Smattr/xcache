5656615b7b35910fe49dded40dc9975db4fde4a7
Matthew Fernandez <matthew.fernandez@gmail.com>
2013-12-08 16:48:56 +1100

    dict: Support passing NULL into find.

--- src/dict.c
+++ src/dict.c
@@ -32,24 +32,26 @@ static unsigned int hash(char *s) {
 }
 
 static entry_t *find(dict_t *dict, char *key, unsigned int index, entry_t **prev) {
-    assert(prev != NULL);
-    *prev = NULL;
-
     assert(dict != NULL);
-    for (entry_t *p = dict[index]; p != NULL; *prev = p, p = p->next) {
+    for (entry_t *p = dict[index], *q = NULL; p != NULL; q = p, p = p->next) {
         if (!strcmp(p->key, key)) {
             /* Found it. */
+            if (prev != NULL) {
+                *prev = q;
+            }
             return p;
         }
     }
     /* Didn't find it. */
+    if (prev != NULL) {
+        *prev = NULL;
+    }
     return NULL;
 }
 
 int dict_add(dict_t *dict, char *key, void *value, void **oldvalue) {
     unsigned int index = hash(key);
-    entry_t *prev;
-    entry_t *e = find(dict, key, index, &prev);
+    entry_t *e = find(dict, key, index, NULL);
     if (e == NULL) {
         e = (entry_t*)malloc(sizeof(entry_t));
         if (e == NULL) {
@@ -93,9 +95,9 @@ void *dict_remove(dict_t *dict, char *key) {
 
 void *dict_find(dict_t *dict, char *key) {
     unsigned int index = hash(key);
-    entry_t *p, *prev;
+    entry_t *p;
 
-    p = find(dict, key, index, &prev);
+    p = find(dict, key, index, NULL);
     if (p != NULL) {
         return p->value;
     }
