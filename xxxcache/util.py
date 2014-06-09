64f3122c4091b507b61e234315f2a7b23e46add8
Matthew Fernandez <matthew.fernandez@gmail.com>
2014-06-08 17:49:12 -0700

    xxxcache: Resolve symlinks when checking executables.

--- xxxcache/util.py
+++ xxxcache/util.py
@@ -6,7 +6,7 @@ def resolve(target, mustnotbe=None):
     syspath = os.environ.get('PATH')
     roots = syspath.split(':') if syspath is not None else []
     for r in roots:
-        candidate = os.path.abspath(os.path.join(r, target))
+        candidate = os.path.realpath(os.path.join(r, target))
         if os.path.isfile(candidate) and os.access(candidate, os.X_OK) and \
                 candidate not in mustnotbe:
             return candidate
