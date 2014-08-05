79476549771b6b63ac1f99e5a6ece288903ce454
Matthew Fernandez <matthew.fernandez@gmail.com>
2014-08-04 20:49:25 -0700

    Add invocation fingerprint functionality.

---
+++ src/fingerprint.c
@@ -0,0 +1,57 @@
+#include "fingerprint.h"
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+fingerprint_t *fingerprint(unsigned int argc, const char **argv) {
+    fingerprint_t *f = calloc(1, sizeof(*f));
+    if (f == NULL)
+        goto fail;
+
+    f->cwd = strdup(getcwd(NULL, 0));
+    if (f->cwd == NULL)
+        goto fail;
+
+    f->arg_lens = calloc(argc, sizeof(*f->arg_lens));
+    if (f->arg_lens == NULL)
+        goto fail;
+    f->arg_lens_sz = argc;
+
+    unsigned int len = 0;
+    for (unsigned int i = 0; i < argc; i++) {
+        f->arg_lens[i] = strlen(argv[i]);
+        if (i != 0)
+            len++;
+        len += f->arg_lens[i];
+    }
+
+    f->argv = malloc(sizeof(char) * (len + 1));
+    if (f->argv == NULL)
+        goto fail;
+
+    char *p = f->argv;
+    for (unsigned int i = 0; i < argc; i++) {
+        if (i > 0)
+            *p++ = ' ';
+        strncpy(p, argv[i], f->arg_lens[i]);
+        p += f->arg_lens[i];
+    }
+    *p = '\0';
+
+    return f;
+
+fail:
+    fingerprint_destroy(f);
+    return NULL;
+}
+
+void fingerprint_destroy(fingerprint_t *fp) {
+    if (fp != NULL) {
+        if (fp->arg_lens != NULL)
+            free(fp->arg_lens);
+        if (fp->cwd != NULL)
+            free(fp->cwd);
+        free(fp);
+    }
+}
---
+++ src/fingerprint.h
@@ -0,0 +1,30 @@
+/* Functions for generating data that uniquely identifies an invocation of a
+ * process with arguments. We want to be able to write the resulting
+ * fingerprint into a database, so all members need to be of formats we can
+ * store in SQLite. Note that the arg_lens field is stored as a BLOB.
+ */
+
+#ifndef _XCACHE_FINGERPRINT_
+#define _XCACHE_FINGERPRINT_
+
+#include <stdint.h>
+
+typedef struct {
+    /* Current working directory */
+    char *cwd;
+
+    /* Characters is each argument */
+    uint32_t *arg_lens;
+    unsigned int arg_lens_sz;
+
+    /* Concatenated arguments */
+    char *argv;
+} fingerprint_t;
+
+/* Create a fingerprint for the given invocation. Returns NULL on failure. */
+fingerprint_t *fingerprint(unsigned int argc, const char **argv);
+
+/* Deallocate memory associated with a fingerprint. */
+void fingerprint_destroy(fingerprint_t *fp);
+
+#endif
