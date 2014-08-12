114bb9b8b074d0e8e8480b433ab9fa3256235e37
Matthew Fernandez <matthew.fernandez@gmail.com>
2014-08-11 20:41:37 -0700

    Add a library for hooking getenv() in the tracee.

---
+++ src/hook-getenv.c
@@ -0,0 +1,65 @@
+/* A library for hooking getenv and conveying any accesses to Xcache. Note that
+ * this file is *NOT* compiled into xcache; it is compiled to a separate
+ & library.
+ *
+ * The expected use case of this is that Xcache is trying to trace a target and
+ * wants to, in addition to observing the target's syscalls, observe the
+ * target's access to environment variables. In order to do this, Xcache can
+ * LD_PRELOAD this library into the target. Note that communication via the
+ * 'out' file needs to be cooperative, in the sense that this library and
+ * Xcache need to speak the same protocol.
+ */
+
+#define _GNU_SOURCE
+#include <assert.h>
+#include <dlfcn.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+static char *(*real_getenv)(const char *name);
+static FILE *out;
+
+static void init(void) {
+    assert(real_getenv == NULL);
+
+    /* Locate the libc getenv. */
+    real_getenv = dlsym(RTLD_NEXT, "getenv");
+    assert(real_getenv != NULL);
+
+    /* Find the file descriptor Xcache is expecting us to communicate on. */
+    char *pipe = real_getenv("XCACHE_PIPE");
+    if (pipe == NULL)
+        /* Not found. Running outside of Xcache? */
+        return;
+    int fd = atoi(pipe);
+    if (fd > 0)
+        /* Found it. */
+        out = fdopen(fd, "w");
+}
+
+/* Write the given (possibly NULL) string to the Xcache pipe. Note that we must
+ * follow the protocol Xcache is expecting for conveying string data.
+ */
+static void output(const char *data) {
+    if (out == NULL)
+        /* We don't have a pipe to communicate with. */
+        return;
+    fputc(data == NULL ? 0 : 1, out);
+    if (data != NULL)
+        fputs(data, out);
+    fputc(0, out);
+}
+
+/* Hooked version of getenv. We lookup environment variables, as expected, but
+ * we also pass any accessed environment variables to Xcache, that we are
+ * expecting to be tracing us.
+ */
+char *getenv(const char *name) {
+    if (real_getenv == NULL)
+        init();
+    assert(real_getenv != NULL);
+    output(name);
+    char *v = real_getenv(name);
+    output(v);
+    return v;
+}
