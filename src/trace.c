adb32127402ada45d58d85eca507786c378aac6e
Matthew Fernandez <matthew.fernandez@gmail.com>
2014-08-16 11:40:23 -0700

    Fix: Use parent's environment when --no-getenv is passed.
    We were accidentally setting an empty environment for the child which was
    causing much unintended behaviour.

--- src/trace.c
+++ src/trace.c
@@ -102,7 +102,7 @@ tracee_t *trace(const char **argv, const char *tracer) {
              * just ignore it and continue without the extra library. This
              * functionality is not critical.
              */
-            char **env = NULL;
+            char **env = environ;
             if (tracer != NULL) {
                 char *lib = locate_hooklib(tracer);
                 if (lib != NULL) {
