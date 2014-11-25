164781f4bfe3da62de0c5aa4dde5f6f323cfd8dc
Matthew Fernandez <matthew.fernandez@gmail.com>
2014-11-25 22:39:16 +1100

    Replace internal unit testing infrastructure with CUnit.

--- src/test.c
+++ src/test.c
@@ -3,76 +3,115 @@
     #undef NDEBUG
 #endif
 
+#ifndef XCACHE_TEST
+    #error This file is not intended to be included in anything other than the test suite
+#endif
+
 #include <assert.h>
-#include <stdbool.h>
+#include <CUnit/Basic.h>
+#include <CUnit/CUnit.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "test.h"
 
-#ifndef XCACHE_TEST
-    #error This file is not intended to be included in anything other than the test suite
-#endif
+/* Some functions that are defined elsewhere with the STATIC macro. */
+extern const char *atom(char **path);
+extern char *append(char *dest, char *src);
 
-/* A linked list to contain the tests. Note, we don't want to use the linked
- * list implementation in utils in case it has bugs, which we may be testing
- * for.
- */
-typedef struct _node {
-    const char *name;
-    int (*fn)(void);
-    struct _node *next;
-} node_t;
-static node_t *tests;
-
-void register_test(const char *name, int (*fn)(void)) {
-    node_t *n = malloc(sizeof(*n));
-    assert(n != NULL);
-    n->name = name;
-    n->fn = fn;
-    n->next = tests;
-    tests = n;
+static void test_atom_nothing(void) {
+    char *s = strdup("");
+    assert(s != NULL);
+    const char *a = atom(&s);
+    CU_ASSERT_PTR_NULL(a);
 }
 
-int main(int argc, char **argv) {
+static void test_atom_slash(void) {
+    char *s = strdup("/");
+    assert(s != NULL);
+    const char *a = atom(&s);
+    CU_ASSERT_PTR_NULL(a);
+}
+
+static void test_atom_slashslash(void) {
+    char *s = strdup("//");
+    assert(s != NULL);
+    const char *a = atom(&s);
+    CU_ASSERT_PTR_NULL(a);
+}
+
+static void test_atom_single(void) {
+    char *s = strdup("hello");
+    assert(s != NULL);
+    const char *a = atom(&s);
+    CU_ASSERT_STRING_EQUAL(a, "hello");
+    a = atom(&s);
+    CU_ASSERT_PTR_NULL(a);
+}
+
+static void test_atom_single_abs(void) {
+    char *s = strdup("/hello");
+    assert(s != NULL);
+    const char *a = atom(&s);
+    CU_ASSERT_STRING_EQUAL(a, "hello");
+    a = atom(&s);
+    CU_ASSERT_PTR_NULL(a);
+}
+
+static void test_atom_single_trailing(void) {
+    char *s = strdup("hello/");
+    assert(s != NULL);
+    const char *a = atom(&s);
+    CU_ASSERT_STRING_EQUAL(a, "hello");
+    a = atom(&s);
+    CU_ASSERT_PTR_NULL(a);
+}
+
+static void test_atom_single_abs_trailing(void) {
+    char *s = strdup("/hello/");
+    assert(s != NULL);
+    const char *a = atom(&s);
+    CU_ASSERT_STRING_EQUAL(a, "hello");
+    a = atom(&s);
+    CU_ASSERT_PTR_NULL(a);
+}
+
+int main(void) {
     printf("Starting xcache test suite...\n");
-    int total = 0;
 
-    /* Loop through all the registered tests. */
-    for (node_t *n = tests; n != NULL; n = n->next) {
-
-        /* Let the user run specific tests by passing a command line list of
-         * them.
-         */
-        if (argc > 1) {
-            bool enabled = false;
-            for (int i = 1; i < argc; i++) {
-                if (!strcmp(argv[i], n->name)) {
-                    enabled = true;
-                    break;
-                }
-            }
-            if (!enabled) {
-                continue;
-            }
-        }
-
-        /* Run the test and report the result. */
-        printf(" %s...", n->name);
-        int result = n->fn();
-        if (result == 0) {
-            printf("OK\n");
-        } else {
-            printf("Failed\n");
-            total++;
-        }
+    if (CU_initialize_registry() != CUE_SUCCESS) {
+        CU_ErrorCode err = CU_get_error();
+        fprintf(stderr, "failed to initialise CUnit registry\n");
+        return err;
     }
 
-    /* Return an exit status representing the test suite result. */
-    if (total > 0) {
-        printf("%d tests failed\n", total);
-        return -1;
+    CU_pSuite suite = CU_add_suite("atom", NULL, NULL);
+    if (suite == NULL) {
+        CU_ErrorCode err = CU_get_error();
+        CU_cleanup_registry();
+        fprintf(stderr, "failed to add suite\n");
+        return err;
     }
-    printf("All tests succeeded\n");
-    return 0;
+    if ((CU_add_test(suite, "atom(\"\")", test_atom_nothing) == NULL) ||
+        (CU_add_test(suite, "atom(\"/\")", test_atom_slash) == NULL) ||
+        (CU_add_test(suite, "atom(\"//\")", test_atom_slashslash) == NULL) ||
+        (CU_add_test(suite, "atom of single word", test_atom_single) == NULL) ||
+        (CU_add_test(suite, "atom of top-level directory", test_atom_single_abs) == NULL) ||
+        (CU_add_test(suite, "atom of single word with trailing slash", test_atom_single_trailing) == NULL) ||
+        (CU_add_test(suite, "atom of top-level directory with trailing slash", test_atom_single_abs_trailing) == NULL)) {
+        CU_ErrorCode err = CU_get_error();
+        fprintf(stderr, "failed to add tests\n");
+        CU_cleanup_registry();
+        return err;
+    }
+
+    CU_basic_set_mode(CU_BRM_VERBOSE);
+    CU_ErrorCode err = CU_basic_run_tests();
+    CU_cleanup_registry();
+
+    if (err == CUE_SUCCESS) {
+        printf("All tests succeeded\n");
+        return 0;
+    }
+    return err;
 }
