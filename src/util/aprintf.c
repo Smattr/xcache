a6140460ccb649887cdf0f5a6e8feb065991df22
Matthew Fernandez <matthew.fernandez@gmail.com>
2015-01-05 21:44:40 +1100

    Reimplement `asprintf` internally.
    This puzzles me, but `asprintf` seems to unconditionally leak memory. Some
    googling suggests this is a known issue and nobody cares. Given that the number
    of times we invoke `asprintf` is dependent on the target we're tracing, we leak
    an unconstrained amount of memory. This makes it a little unsuitable for
    xcache.

--- src/util/aprintf.c
+++ src/util/aprintf.c
@@ -1,16 +1,34 @@
-/* Convenience wrapper around asprintf. */
+/* asprintf-alike. As far as valgrind can tell, asprintf leaks memory, which
+ * makes it a little dangerous to call in the unconstrained context of xcache.
+ */
 
-#define _GNU_SOURCE
+#include <assert.h>
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 char *aprintf(const char *fmt, ...) {
     va_list ap;
     va_start(ap, fmt);
-    char *strp;
-    int err = vasprintf(&strp, fmt, ap);
+
+    /* We need to copy the va_list because a call to vsnprintf invalidates it
+     * and we need to invoke it twice.
+     */
+    va_list ap_copy;
+    va_copy(ap_copy, ap);
+
+    /* Figure out how many bytes we need for this string. */
+    size_t len = vsnprintf(NULL, 0, fmt, ap);
     va_end(ap);
-    if (err == -1)
+
+    char *s = malloc(sizeof(char) * (len + 1));
+    if (s == NULL)
         return NULL;
-    return strp;
+
+    /* OK, let's do this thing. */
+    size_t len2 __attribute__((unused)) = vsprintf(s, fmt, ap_copy);
+    assert(len2 == len);
+    va_end(ap_copy);
+
+    return s;
 }
