7737b2ea54bd79fd61398c1edea50a6de0445021
Matthew Fernandez <matthew.fernandez@gmail.com>
2015-04-06 12:10:11 +1000

    Doxygen-ise util.h

--- src/util.h
+++ src/util.h
@@ -1,4 +1,6 @@
-/* Various miscellaneous utility functions. None of this is xcache-specific. */
+/** \file Various miscellaneous utility functions. None of this is
+ * xcache-specific.
+ */
 
 #ifndef _XCACHE_UTIL_H_
 #define _XCACHE_UTIL_H_
@@ -9,80 +11,92 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Join two paths and normalise the result.
+/** \brief Join two paths and normalise the result.
  *
- * dest - Path prefix. This is assumed to already be normalised and point to at
- *   least PATH_MAX bytes of available memory.
- * src - Path suffix.
+ * @param dest Path prefix. This is assumed to already be normalised and point
+ *   to at least `PATH_MAX` bytes of available memory.
+ * @param src Path suffix.
  */
 void normpath(char *dest, char *src);
 
-/* Resolve and normalise a path. This does the equivalent of `realpath`, but
- * does not require the resulting path to exist. Note that it does not resolve
- * symlinks.
+/** \brief Resolve and normalise a path.
  *
- * cwd - Root for the relative path to be resolved from.
- * relpath - A relative or absolute path to normalise.
+ * This does the equivalent of `realpath`, but does not require the resulting
+ * path to exist. Note that it does not resolve symlinks.
  *
- * Returns an absolute path or NULL on error.
+ * @param cwd Root for the relative path to be resolved from.
+ * @param relpath A relative or absolute path to normalise.
+ * @return An absolute path or `NULL` on error.
  */
 char *abspath(const char *cwd, char *relpath) __attribute__((nonnull));
 
-/* A version of sprintf that does allocation internally for convenience. */
+/** \brief A version of `sprintf` that does allocation internally for
+ * convenience.
+ *
+ * @param fmt printf-style format string.
+ * @return The sprintf-ed string or `NULL` on allocation failure.
+ */
 char *aprintf(const char *fmt, ...);
 
-/* Return the hash of the contents of a file. The caller should not rely on any
- * property of the hash except it being deterministic and printable. This
- * includes that the caller should not assume a particular hashing algorithm is
- * in use.
+/** \brief Return the hash of the contents of a file.
  *
- * filename - An absolute path to the file to hash.
+ * The caller should not rely on any property of the hash except it being
+ * deterministic and printable. This includes that the caller should not assume
+ * a particular hashing algorithm is in use.
  *
- * Returns a pointer to the hash or NULL on failure. It is the caller's
- * responsibility to free the returned pointer.
+ * @param filename An absolute path to the file to hash.
+ * @return A pointer to the hash or `NULL` on failure. It is the caller's
+ *   responsibility to free the returned pointer.
  */
 char *filehash(const char *filename);
 
-/* Copy a file, preserving the permissions, owner and group if possible.
+/** \brief Copy a file, preserving the permissions, owner and group if
+ * possible.
  *
- * from - Absolute path of source.
- * to - Absolute path of destination.
- *
- * Returns 0 on success, -1 on failure.
+ * @param from Absolute path of source.
+ * @param to Absolute path of destination.
+ * @return 0 on success, -1 on failure.
  */
 int cp(const char *from, const char *to);
 
-/* Equivalent of `mkdir -p`.
+/** \brief Equivalent of `mkdir -p`.
  *
- * path - An absolute or relative path to the final directory to create.
- *
- * Returns 0 on success, -1 on failure.
+ * @param path An absolute or relative path to the final directory to create.
+ * @return 0 on success, -1 on failure.
  */
 int mkdirp(const char *path);
 
-/* Equivalent of `du -cS`.
+/** \brief Equivalent of `du -cS`.
  * 
- * path - An absolute or relative path to a directory whose contents to
+ * @param path An absolute or relative path to a directory whose contents to
  *   measure. Note that subdirectories and special files are not taken into
  *   account.
- *
- * Return -1 on failure or the total size of files in the directory on success.
+ * @return -1 on failure or the total size of files in the directory on
+ *   success.
  */
 ssize_t du(const char *path);
 
-/* Remove files within a directory up to a certain limit.
+/** \brief Remove files within a directory up to a certain limit.
  *
- * path - An absolute or relative path to the directory to prune entries from.
- * reduction - Remove files until we have removed at least this many bytes.
- *
- * Returns the number of bytes that were removed.
+ * @param path An absolute or relative path to the directory to prune entries
+ *   from.
+ * @param reduction Remove files until we have removed at least this many
+ *   bytes.
+ * @return The number of bytes that were removed.
  */
 size_t reduce(const char *path, size_t reduction);
 
-/* fgets-/getdelim-alike. It's possible I'm too tired or dense at this point,
- * but all the POSIX functions I'm aware of seem to have some deal-breaking
- * flaw for reading a '\0'-terminated string into a static buffer. Perhaps
- * reading programmatic strings from a file descriptor is not a common task.
+/** \brief fgets-/getdelim-alike.
+ *
+ * It's possible I'm too tired or dense at this point, but all the POSIX
+ * functions I'm aware of seem to have some deal-breaking flaw for reading a
+ * '\0'-terminated string into a static buffer. Perhaps reading programmatic
+ * strings from a file descriptor is not a common task.
+ *
+ * @param buffer Destination to read into.
+ * @param limit Maximum bytes to read.
+ * @param f Source file descriptor to read from.
+ * @return true if data was read, false on EOF or no bytes read.
  */
 bool get(char *buffer, size_t limit, FILE *f);
 
@@ -91,12 +105,20 @@ file_iter_t *file_iter(const char *path);
 void file_iter_destroy(file_iter_t *fi);
 char *file_iter_next(file_iter_t *fi);
 
-/* Convenience wrapper around realloc that frees the first argument if
- * allocation fails.
+/** \brief Convenience wrapper around `realloc` that frees the first argument
+ * if allocation fails.
+ *
+ * @param ptr Initial pointer.
+ * @param size Number of bytes to allocate for the new area.
+ * @return New pointer to use or `NULL` on failure.
  */
 void *ralloc(void *ptr, size_t size);
 
-/* An alternative to readlink that NUL-terminates the output buffer. */
+/** \brief An alternative to readlink that NUL-terminates the output buffer.
+ *
+ * @param path Symlink to read target of.
+ * @return Target path of the symlink.
+ */
 char *readln(const char *path);
 
 #endif
