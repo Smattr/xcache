1385b6c403bcc4de8c586385315f315116630efd
Matthew Fernandez <matthew.fernandez@gmail.com>
2016-02-27 17:51:13 +1100

    Run tests in Debug and Release mode.

--- tests/run-all.py
+++ tests/run-all.py
@@ -23,50 +23,53 @@ def main():
     me = os.path.abspath(__file__)
     my_dir = os.path.dirname(me)
 
-    sys.stderr.write('Building all targets... ')
-    sys.stderr.flush()
-    src = os.path.abspath(os.path.join(my_dir, '../src'))
-    tmp = tempfile.mkdtemp()
-    atexit.register(shutil.rmtree, tmp)
-    ret = run(['cmake', src, '-G', 'Ninja'], cwd=tmp)
-    if ret != 0:
-        show_failed()
-        return ret
-    ret = run(['ninja'], cwd=tmp)
-    if ret == 0:
-        show_passed()
-    else:
-        # If this fails it's critical; bail out
-        show_failed()
-        return ret
-
     failed = False
+    src = os.path.abspath(os.path.join(my_dir, '../src'))
 
-    sys.stderr.write('Running unit tests... ')
-    ret = run([os.path.join(tmp, 'xcache-tests')])
-    if ret == 0:
-        show_passed()
-    else:
-        show_failed()
-        failed = True
+    for mode in ('Debug', 'Release'):
 
-    env = copy.deepcopy(os.environ)
-    env['PATH'] = '%s:%s' % (env.get('PATH', ''), tmp)
-    env['LD_LIBRARY_PATH'] = tmp
+        sys.stderr.write('Building all targets in %s mode... ' % mode)
+        sys.stderr.flush()
+        tmp = tempfile.mkdtemp()
+        atexit.register(shutil.rmtree, tmp)
+        ret = run(['cmake', src, '-DCMAKE_BUILD_TYPE=%s' % mode, '-G', 'Ninja'],
+            cwd=tmp)
+        if ret != 0:
+            show_failed()
+            return ret
+        ret = run(['ninja'], cwd=tmp)
+        if ret == 0:
+            show_passed()
+        else:
+            # If this fails it's critical; bail out
+            show_failed()
+            return ret
 
-    for t in os.listdir(my_dir):
-        test = os.path.abspath(os.path.join(my_dir, t))
-        if os.path.isfile(test) and os.access(test, os.X_OK) and test != me:
-            sys.stderr.write('Running %s... ' % test)
-            sys.stderr.flush()
-            cwd = tempfile.mkdtemp()
-            atexit.register(shutil.rmtree, cwd)
-            ret = run([test], env=env, cwd=cwd)
-            if ret == 0:
-                show_passed()
-            else:
-                show_failed()
-                failed = True
+        sys.stderr.write('Running unit tests... ')
+        ret = run([os.path.join(tmp, 'xcache-tests')])
+        if ret == 0:
+            show_passed()
+        else:
+            show_failed()
+            failed = True
+
+        env = copy.deepcopy(os.environ)
+        env['PATH'] = '%s:%s' % (env.get('PATH', ''), tmp)
+        env['LD_LIBRARY_PATH'] = tmp
+
+        for t in os.listdir(my_dir):
+            test = os.path.abspath(os.path.join(my_dir, t))
+            if os.path.isfile(test) and os.access(test, os.X_OK) and test != me:
+                sys.stderr.write('Running %s... ' % test)
+                sys.stderr.flush()
+                cwd = tempfile.mkdtemp()
+                atexit.register(shutil.rmtree, cwd)
+                ret = run([test], env=env, cwd=cwd)
+                if ret == 0:
+                    show_passed()
+                else:
+                    show_failed()
+                    failed = True
 
     if failed:
         return -1
