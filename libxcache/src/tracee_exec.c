138295f327255e3eca95b6a9f66ec9cc9c7a971f
Matthew Fernandez <matthew.fernandez@gmail.com>
2021-11-26 13:53:47 -0800

    support for debug messages from the child context

--- libxcache/src/tracee_exec.c
+++ libxcache/src/tracee_exec.c
@@ -1,4 +1,5 @@
 #include "channel.h"
+#include "debug.h"
 #include "macros.h"
 #include "tracee.h"
 #include <assert.h>
@@ -8,6 +9,7 @@
 #include <linux/filter.h>
 #include <linux/seccomp.h>
 #include <signal.h>
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <sys/prctl.h>
@@ -16,6 +18,16 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <xcache/proc.h>
+
+// flip this to true when debugging tricky child startup failures
+enum { DEBUGGING = false };
+
+#define DEBUG_(args...)                                                        \
+  do {                                                                         \
+    if (DEBUGGING) {                                                           \
+      DEBUG("[tracee] " args);                                                 \
+    }                                                                          \
+  } while (0)
 
 // The tracee’s use of the message pipe is coordinated with the tracer. That is,
 // the interleaving of the tracee’s writes to `tracee->msg` with other
@@ -42,16 +54,22 @@ static int exec(tracee_t *tracee) {
   // replace our streams with pipes to the parent
   if (UNLIKELY(dup2(tracee->out[0], STDOUT_FILENO) == -1))
     return errno;
-  if (UNLIKELY(dup2(tracee->err[1], STDERR_FILENO) == -1))
+  if (!DEBUGGING && UNLIKELY(dup2(tracee->err[1], STDERR_FILENO) == -1))
     return errno;
 
   // opt-in to being a ptrace tracee
-  if (UNLIKELY(ptrace(PTRACE_TRACEME, 0, NULL, NULL) != 0))
-    return errno;
+  if (UNLIKELY(ptrace(PTRACE_TRACEME, 0, NULL, NULL) != 0)) {
+    int r = errno;
+    DEBUG_("failed to opt-in to ptrace: %d", r);
+    return r;
+  }
 
   // set no-new-privs so we can install a seccomp filter without CAP_SYS_ADMIN
-  if (UNLIKELY(prctl(PR_SET_NO_NEW_PRIVS, 1, 0, 0, 0) == -1))
-    return errno;
+  if (UNLIKELY(prctl(PR_SET_NO_NEW_PRIVS, 1, 0, 0, 0) == -1)) {
+    int r = errno;
+    DEBUG_("failed to set no-new-privs: %d", r);
+    return r;
+  }
 
   // load a seccomp filter that intercepts relevant syscalls
   static struct sock_filter filter[] = {
@@ -67,14 +85,29 @@ static int exec(tracee_t *tracee) {
       .filter = filter,
       .len = sizeof(filter) / sizeof(filter[0]),
   };
-  if (UNLIKELY(prctl(PR_SET_SECCOMP, SECCOMP_MODE_FILTER, &prog, 0, 0) == -1))
-    return errno;
+  if (UNLIKELY(prctl(PR_SET_SECCOMP, SECCOMP_MODE_FILTER, &prog, 0, 0) == -1)) {
+    int r = errno;
+    DEBUG_("failed to install seccomp filter: %d", r);
+    return r;
+  }
 
   // signal to out parent that we passed phase 1 of our setup
-  (void)channel_write(&tracee->msg, 0);
+  {
+    int r = channel_write(&tracee->msg, 0);
+    if (UNLIKELY(r != 0)) {
+      DEBUG_("failed to notify parent of phase 1 completion: %d", r);
+      // ignore as there is nothing else we can do
+    }
+  }
 
   // give our parent an opportunity to attach to us
-  (void)raise(SIGSTOP);
+  {
+    int r = raise(SIGSTOP);
+    if (UNLIKELY(r != 0)) {
+      DEBUG_("failed to SIGSTOP ourselves: %d", r);
+      // ignore as there is nothing else we can do
+    }
+  }
 
   return xc_proc_exec(tracee->proc);
 }
