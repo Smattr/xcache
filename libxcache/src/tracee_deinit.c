19f7341a349b905f7516f065ba6290458122701d
Matthew Fernandez <matthew.fernandez@gmail.com>
2021-11-28 16:16:34 -0800

    tracee_deinit: more uniform style

--- libxcache/src/tracee_deinit.c
+++ libxcache/src/tracee_deinit.c
@@ -14,17 +14,15 @@ void tracee_deinit(tracee_t *tracee) {
   // TODO what to do if the tracee is still live (pid > 0)?
 
   for (size_t i = 0; i < sizeof(tracee->out) / sizeof(tracee->out[0]); ++i) {
-    if (tracee->out[i] > 0) {
+    if (tracee->out[i] > 0)
       (void)close(tracee->out[i]);
-      tracee->out[i] = 0;
-    }
+    tracee->out[i] = 0;
   }
 
   for (size_t i = 0; i < sizeof(tracee->err) / sizeof(tracee->err[0]); ++i) {
-    if (tracee->err[i] > 0) {
+    if (tracee->err[i] > 0)
       (void)close(tracee->err[i]);
-      tracee->err[i] = 0;
-    }
+    tracee->err[i] = 0;
   }
 
   if (tracee->out_content != NULL)
