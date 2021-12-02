0dc10fedc7f5f61c1b2e008c0b1ea0f4759acea3
Matthew Fernandez <matthew.fernandez@gmail.com>
2021-12-01 18:46:57 -0800

    implement make file API on database itself

--- libxcache/src/db.h
+++ libxcache/src/db.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include "macros.h"
 #include <sqlite3.h>
+#include <stdio.h>
 #include <xcache/db.h>
 
 struct xc_db {
@@ -14,3 +16,11 @@ struct xc_db {
   /// was the database opened read-only?
   unsigned read_only : 1;
 };
+
+/// create a file for capturing a trace output into
+///
+/// \param db Database in which to home the trace output
+/// \param fp [out] Write handle to the created file on success
+/// \param path [out] Path to the created file on success
+/// \return 0 on success or an errno on failure
+INTERNAL int db_make_file(xc_db_t *db, FILE **fp, char **path);
---
+++ libxcache/src/db_make_file.c
@@ -0,0 +1,56 @@
+#include "db.h"
+#include "macros.h"
+#include "path.h"
+#include <assert.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int db_make_file(xc_db_t *db, FILE **fp, char **path) {
+
+  assert(db != NULL);
+  assert(fp != NULL);
+  assert(path != NULL);
+
+  int rc = 0;
+  int fd = -1;
+  FILE *f = NULL;
+
+  // construct a template file path pointing into the database root
+  char *template = path_join(db->root, "data.XXXXXX");
+  if (UNLIKELY(template == NULL)) {
+    rc = ENOMEM;
+    goto done;
+  }
+
+  // create a file at a new unique path
+  fd = mkstemp(template);
+  if (UNLIKELY(fd == -1)) {
+    rc = errno;
+    goto done;
+  }
+
+  // turn the descriptor into a file handle
+  f = fdopen(fd, "r");
+  if (UNLIKELY(f == NULL)) {
+    rc = errno;
+    goto done;
+  }
+
+  *fp = f;
+  *path = template;
+
+done:
+  if (UNLIKELY(rc != 0)) {
+    if (f != NULL) {
+      (void)fclose(f);
+    } else if (fd >= 0) {
+      (void)close(fd);
+      (void)unlink(template);
+    }
+    free(template);
+  }
+
+  return rc;
+}
