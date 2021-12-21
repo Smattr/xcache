a64b17bf24c4b02dcff0c9563236af96d1aa186b
Matthew Fernandez <matthew.fernandez@gmail.com>
2021-12-20 20:08:50 -0800

    flip tracee_deinit to LIFO

--- libxcache/src/tracee_deinit.c
+++ libxcache/src/tracee_deinit.c
@@ -11,15 +11,23 @@ void tracee_deinit(tracee_t *tracee) {
 
   trace_deinit(&tracee->trace);
 
-  tracee->proc = NULL;
+  if (tracee->err_path != NULL)
+    (void)unlink(tracee->err_path);
+  free(tracee->err_path);
+  tracee->err_path = NULL;
 
-  // TODO what to do if the tracee is still live (pid > 0)?
+  if (tracee->err_f != NULL)
+    (void)fclose(tracee->err_f);
+  tracee->err_f = NULL;
 
-  for (size_t i = 0; i < sizeof(tracee->out) / sizeof(tracee->out[0]); ++i) {
-    if (tracee->out[i] > 0)
-      (void)close(tracee->out[i]);
-    tracee->out[i] = 0;
-  }
+  if (tracee->out_path != NULL)
+    (void)unlink(tracee->out_path);
+  free(tracee->out_path);
+  tracee->out_path = NULL;
+
+  if (tracee->out_f != NULL)
+    (void)fclose(tracee->out_f);
+  tracee->out_f = NULL;
 
   for (size_t i = 0; i < sizeof(tracee->err) / sizeof(tracee->err[0]); ++i) {
     if (tracee->err[i] > 0)
@@ -27,23 +35,11 @@ void tracee_deinit(tracee_t *tracee) {
     tracee->err[i] = 0;
   }
 
-  if (tracee->out_f != NULL)
-    (void)fclose(tracee->out_f);
-  tracee->out_f = NULL;
-
-  if (tracee->out_path != NULL)
-    (void)unlink(tracee->out_path);
-  free(tracee->out_path);
-  tracee->out_path = NULL;
-
-  if (tracee->err_f != NULL)
-    (void)fclose(tracee->err_f);
-  tracee->err_f = NULL;
-
-  if (tracee->err_path != NULL)
-    (void)unlink(tracee->err_path);
-  free(tracee->err_path);
-  tracee->err_path = NULL;
+  for (size_t i = 0; i < sizeof(tracee->out) / sizeof(tracee->out[0]); ++i) {
+    if (tracee->out[i] > 0)
+      (void)close(tracee->out[i]);
+    tracee->out[i] = 0;
+  }
 
   while (tracee->fds != NULL) {
     open_file_t *of = tracee->fds;
@@ -54,4 +50,8 @@ void tracee_deinit(tracee_t *tracee) {
 
   free(tracee->cwd);
   tracee->cwd = NULL;
+
+  // TODO what to do if the tracee is still live (pid > 0)?
+
+  tracee->proc = NULL;
 }
