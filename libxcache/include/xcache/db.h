653c057adaca0e7c9133881c32581fff78c4f157
Matthew Fernandez <matthew.fernandez@gmail.com>
2021-12-30 14:06:35 -0800

    xc_db_save: make 'db' argument non-const
    While the usage within this function is consistent with const, morally the
    semantics are non-const (we are _writing_ to the database). So this makes
    calling this function a little more ergonomic.
    
    This reverts part of 18f609531c3a02d0b8a67e0ca6455ef62fdbfdfb.

--- libxcache/include/xcache/db.h
+++ libxcache/include/xcache/db.h
@@ -34,7 +34,7 @@ XCACHE_API int xc_db_load(const xc_db_t *db, const xc_proc_t *question,
 /// \param proc Process that was recorded
 /// \param observation Recorded trace of a process
 /// \return 0 on success or an errno on failure
-XCACHE_API int xc_db_save(const xc_db_t *db, const xc_proc_t *proc,
+XCACHE_API int xc_db_save(xc_db_t *db, const xc_proc_t *proc,
                           const xc_trace_t *observation);
 
 /// close an open trace database
