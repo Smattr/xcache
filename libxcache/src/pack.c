f1b479b81bd1d9cab8698ecbf295eb44b65cf220
Matthew Fernandez <matthew.fernandez@gmail.com>
2021-12-30 15:35:42 -0800

    implement trace deserialisation

--- libxcache/src/pack.c
+++ libxcache/src/pack.c
@@ -1,6 +1,8 @@
 #include "pack.h"
 #include "debug.h"
 #include "fs_set.h"
+#include "hash.h"
+#include "macros.h"
 #include "proc.h"
 #include "trace.h"
 #include <assert.h>
@@ -8,10 +10,11 @@
 #include <errno.h>
 #include <limits.h>
 #include <stdbool.h>
-#include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/mman.h>
 #include <xcache/proc.h>
 #include <xcache/trace.h>
 
@@ -253,4 +256,372 @@ int pack_trace(FILE *f, const xc_trace_t *trace) {
   }
 
   return 0;
+}
+
+static int read_u8(const void **base, size_t *size, uint8_t *value) {
+  if (ERROR(*size < sizeof(*value)))
+    return ENODATA;
+  memcpy(value, base, sizeof(*value));
+  *base += sizeof(*value);
+  *size -= sizeof(*value);
+  return 0;
+}
+
+static int read_u16(const void **base, size_t *size, uint16_t *value) {
+  if (ERROR(*size < sizeof(*value)))
+    return ENODATA;
+  uint16_t v = 0;
+  memcpy(&v, base, sizeof(v));
+  *value = be16toh(v);
+  *base += sizeof(*value);
+  *size -= sizeof(*value);
+  return 0;
+}
+
+static int read_u32(const void **base, size_t *size, uint32_t *value) {
+  if (ERROR(*size < sizeof(*value)))
+    return ENODATA;
+  uint32_t v = 0;
+  memcpy(&v, base, sizeof(v));
+  *value = be32toh(v);
+  *base += sizeof(*value);
+  *size -= sizeof(*value);
+  return 0;
+}
+
+static int read_u64(const void **base, size_t *size, uint64_t *value) {
+  if (ERROR(*size < sizeof(*value)))
+    return ENODATA;
+  uint64_t v = 0;
+  memcpy(&v, base, sizeof(v));
+  *value = be64toh(v);
+  *base += sizeof(*value);
+  *size -= sizeof(*value);
+  return 0;
+}
+
+static int unpack_array_length(const void **base, size_t *size,
+                               size_t *length) {
+
+  uint8_t byte = 0;
+  {
+    int r = read_u8(base, size, &byte);
+    if (ERROR(r != 0))
+      return r;
+  }
+
+  if (byte >= 0x80 && byte <= 0x80 + 0x17) {
+    *length = byte - 0x80;
+    return 0;
+  }
+
+  switch (byte) {
+
+  case 0x98: {
+    uint8_t l = 0;
+    int r = read_u8(base, size, &l);
+    if (ERROR(r != 0))
+      return r;
+    *length = l;
+    break;
+  }
+
+  case 0x99: {
+    uint16_t l = 0;
+    int r = read_u16(base, size, &l);
+    if (ERROR(r != 0))
+      return r;
+    *length = l;
+    break;
+  }
+
+  case 0x9a: {
+    uint32_t l = 0;
+    int r = read_u32(base, size, &l);
+    if (ERROR(r != 0))
+      return r;
+    *length = l;
+    break;
+  }
+
+  case 0x9b: {
+    uint64_t l = 0;
+    int r = read_u64(base, size, &l);
+    if (ERROR(r != 0))
+      return r;
+    *length = l;
+    break;
+  }
+
+  default:
+    return ENOMSG;
+  }
+
+  return 0;
+}
+
+static int unpack_string(const void **base, size_t *size, char **value) {
+
+  uint8_t byte = 0;
+  {
+    int r = read_u8(base, size, &byte);
+    if (ERROR(r != 0))
+      return r;
+  }
+
+  size_t length = 0;
+
+  switch (byte) {
+
+  case 0xf6:
+    *value = NULL;
+    return 0;
+
+  case 0x78: {
+    uint8_t l = 0;
+    int r = read_u8(base, size, &l);
+    if (ERROR(r != 0))
+      return r;
+    length = l;
+    break;
+  }
+
+  case 0x79: {
+    uint16_t l = 0;
+    int r = read_u16(base, size, &l);
+    if (ERROR(r != 0))
+      return r;
+    length = l;
+    break;
+  }
+
+  case 0x7a: {
+    uint32_t l = 0;
+    int r = read_u32(base, size, &l);
+    if (ERROR(r != 0))
+      return r;
+    length = l;
+    break;
+  }
+
+  case 0x7b: {
+    uint64_t l = 0;
+    int r = read_u64(base, size, &l);
+    if (ERROR(r != 0))
+      return r;
+    length = l;
+    break;
+  }
+
+  default:
+    if (byte >= 0x60 && byte <= 0x60 + 0x17) {
+      length = byte - 0x60;
+      break;
+    }
+    return ENOMSG;
+  }
+
+  if (length > *size)
+    return ENODATA;
+
+  char *v = strndup(*base, length);
+  if (ERROR(v == NULL))
+    return ENOMEM;
+  *base += length;
+  *size -= length;
+
+  *value = v;
+
+  return 0;
+}
+
+static int unpack_bool(const void **base, size_t *size, bool *value) {
+
+  uint8_t byte = 0;
+  {
+    int r = read_u8(base, size, &byte);
+    if (ERROR(r != 0))
+      return r;
+  }
+
+  if (byte == 0xf5) {
+    *value = true;
+  } else if (byte == 0xf4) {
+    *value = false;
+  } else {
+    return ENOMSG;
+  }
+
+  return 0;
+}
+
+static int unpack_uint(const void **base, size_t *size, uintmax_t *value) {
+
+  uint8_t byte = 0;
+  {
+    int r = read_u8(base, size, &byte);
+    if (ERROR(r != 0))
+      return r;
+  }
+
+  if (byte <= 0x00 + 0x17) {
+    *value = byte;
+    return 0;
+  }
+
+  switch (byte) {
+
+  case 0x18: {
+    uint8_t v = 0;
+    int r = read_u8(base, size, &v);
+    if (ERROR(r != 0))
+      return r;
+    *value = v;
+    break;
+  }
+
+  case 0x19: {
+    uint16_t v = 0;
+    int r = read_u16(base, size, &v);
+    if (ERROR(r != 0))
+      return r;
+    *value = v;
+    break;
+  }
+
+  case 0x1a: {
+    uint32_t v = 0;
+    int r = read_u32(base, size, &v);
+    if (ERROR(r != 0))
+      return r;
+    *value = v;
+    break;
+  }
+
+  case 0x1b: {
+    uint64_t v = 0;
+    int r = read_u64(base, size, &v);
+    if (ERROR(r != 0))
+      return r;
+    *value = v;
+    break;
+  }
+
+  default:
+    return ENOMSG;
+  }
+
+  return 0;
+}
+
+static int unpack_tag(const void **base, size_t *size, uint8_t *tag) {
+  return read_u8(base, size, tag);
+}
+
+int unpack_trace(const void *base, size_t size, xc_trace_t **trace) {
+
+  assert(base != NULL && base != MAP_FAILED);
+  assert(trace != NULL);
+
+  // check the tag
+  {
+    uint8_t tag;
+    int r = unpack_tag(&base, &size, &tag);
+    if (ERROR(r != 0))
+      return r;
+    if (ERROR(tag != TRACE_TAG))
+      return ENOMSG;
+  }
+
+  xc_trace_t *t = NULL;
+  int rc = -1;
+
+  t = calloc(1, sizeof(*t));
+  if (ERROR(t == NULL)) {
+    rc = ENOMEM;
+    goto done;
+  }
+
+  // deserialise length of accessed files
+  {
+    size_t length = 0;
+    rc = unpack_array_length(&base, &size, &length);
+    if (ERROR(rc != 0))
+      goto done;
+    t->io.base = calloc(length, sizeof(t->io.base[0]));
+    if (UNLIKELY(length > 0 && t->io.base == NULL)) {
+      rc = ENOMEM;
+      goto done;
+    }
+    t->io.size = length;
+    t->io.capacity = length;
+  }
+
+  // deserialise the accessed files themselves
+  for (size_t i = 0; i < t->io.size; ++i) {
+    fs_t *fs = &t->io.base[i];
+    rc = unpack_string(&base, &size, &fs->path);
+    if (ERROR(rc != 0))
+      goto done;
+    {
+      bool v = false;
+      rc = unpack_bool(&base, &size, &v);
+      if (ERROR(rc != 0))
+        goto done;
+      fs->read = v;
+    }
+    {
+      bool v = false;
+      rc = unpack_bool(&base, &size, &v);
+      if (ERROR(rc != 0))
+        goto done;
+      fs->existed = v;
+    }
+    {
+      bool v = false;
+      rc = unpack_bool(&base, &size, &v);
+      if (ERROR(rc != 0))
+        goto done;
+      fs->accessible = v;
+    }
+    {
+      bool v = false;
+      rc = unpack_bool(&base, &size, &v);
+      if (ERROR(rc != 0))
+        goto done;
+      fs->written = v;
+    }
+    {
+      uintmax_t v = 0;
+      rc = unpack_uint(&base, &size, &v);
+      if (ERROR(rc != 0))
+        goto done;
+      if (v > HASH_MAX) {
+        rc = ENOMSG;
+        goto done;
+      }
+      fs->hash = (xc_hash_t)v;
+    }
+    rc = unpack_string(&base, &size, &fs->content_path);
+    if (ERROR(rc != 0))
+      goto done;
+  }
+
+  // success
+  *trace = t;
+  t = NULL;
+  rc = 0;
+
+done:
+  if (UNLIKELY(t != NULL)) {
+    for (size_t i = 0; i < t->io.size; ++i) {
+      fs_t *fs = &t->io.base[i];
+      free(fs->path);
+      free(fs->content_path);
+    }
+    free(t->io.base);
+  }
+  free(t);
+
+  return rc;
 }
--- libxcache/src/pack.h
+++ libxcache/src/pack.h
@@ -18,3 +18,11 @@ INTERNAL int pack_proc(FILE *f, const xc_proc_t *proc);
 /// \param trace Trace to serialise
 /// \return 0 on success or an errno on failure
 INTERNAL int pack_trace(FILE *f, const xc_trace_t *trace);
+
+/// deserialise a trace
+///
+/// \param base Buffer from which to parse a trace
+/// \param size Number of bytes in `buffer`
+/// \param trace [out] Deserialised trace on success
+/// \return 0 on success or an errno on failure
+INTERNAL int unpack_trace(const void *base, size_t size, xc_trace_t **trace);
