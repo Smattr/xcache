d8c3b05e32f5dae34840638281ed23394efc5e08
Matthew Fernandez <matthew.fernandez@gmail.com>
2021-12-30 11:31:55 -0800

    implement trace localisation

--- libxcache/src/trace.h
+++ libxcache/src/trace.h
@@ -2,6 +2,7 @@
 
 #include "fs_set.h"
 #include "macros.h"
+#include <xcache/db.h>
 #include <xcache/trace.h>
 
 struct xc_trace {
@@ -12,6 +13,18 @@ struct xc_trace {
   /// status the process returned on exit
   int exit_status;
 };
+
+/// localise a trace, making its content paths relative to a database root
+///
+/// A pre-condition is that the content paths of all accessed files are already
+/// known relative to the database root.
+///
+/// \param db Database to make content paths relative to
+/// \param local [out] Localised trace on success
+/// \param global Trace to localise
+/// \return 0 on success or an errno on failure
+INTERNAL int trace_localise(const xc_db_t *db, xc_trace_t **local,
+                            const xc_trace_t *global);
 
 /// clean up and deallocate the members of a trace
 INTERNAL void trace_deinit(xc_trace_t *trace);
---
+++ libxcache/src/trace_localise.c
@@ -0,0 +1,83 @@
+#include "db.h"
+#include "debug.h"
+#include "macros.h"
+#include "path.h"
+#include "trace.h"
+#include <assert.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <xcache/db.h>
+#include <xcache/trace.h>
+
+int trace_localise(const xc_db_t *db, xc_trace_t **local,
+                   const xc_trace_t *global) {
+
+  assert(local != NULL);
+  assert(global != NULL);
+
+  *local = NULL;
+  int rc = -1;
+
+  xc_trace_t *t = calloc(1, sizeof(*t));
+  if (ERROR(t == NULL)) {
+    rc = ENOMEM;
+    goto done;
+  }
+
+  // setup space to copy the files accessed list
+  t->io.base = calloc(global->io.size, sizeof(t->io.base[0]));
+  if (ERROR(global->io.size > 0 && t->io.base == NULL)) {
+    rc = ENOMEM;
+    goto done;
+  }
+  t->io.size = global->io.size;
+  t->io.capacity = global->io.size;
+
+  // copy the entries, making paths relative to the database root
+  for (size_t i = 0; i < t->io.size; ++i) {
+    fs_t *dst = &t->io.base[i];
+    const fs_t *src = &global->io.base[i];
+    assert(src->path != NULL);
+    dst->path = strdup(src->path);
+    if (ERROR(dst->path == NULL)) {
+      rc = ENOMEM;
+      goto done;
+    }
+    dst->read = src->read;
+    dst->existed = src->existed;
+    dst->accessible = src->accessible;
+    dst->written = src->written;
+    dst->hash = src->hash;
+
+    if (src->content_path != NULL) {
+      assert(path_is_relative_to(src->content_path, db->root));
+      size_t stem_offset = strlen(db->root) + 1 /* for '/' */;
+      dst->content_path = strdup(src->content_path + stem_offset);
+      if (ERROR(dst->content_path == NULL)) {
+        rc = ENOMEM;
+        goto done;
+      }
+    }
+  }
+
+  // copy the exit status
+  t->exit_status = global->exit_status;
+
+  // success
+  *local = t;
+  t = NULL;
+  rc = 0;
+
+done:
+  if (UNLIKELY(t != NULL)) {
+    for (size_t i = 0; i < t->io.size; ++i) {
+      free(t->io.base[i].path);
+      free(t->io.base[i].content_path);
+    }
+    free(t->io.base);
+  }
+  free(t);
+
+  return rc;
+}
