7c528d345c16a59997ef6169991f5f4849e8c42e
Matthew Fernandez <matthew.fernandez@gmail.com>
2021-12-29 17:25:29 -0800

    xc_db_open: strip trailing slashes from the cache root
    This should simplify some later operations.

--- libxcache/src/db_open.c
+++ libxcache/src/db_open.c
@@ -1,11 +1,24 @@
 #include "db.h"
 #include "debug.h"
+#include <assert.h>
 #include <errno.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <xcache/db.h>
+
+/// make a canonical path, lacking redundant trailing slashes
+static char *make_canon(const char *path) {
+
+  assert(path != NULL);
+
+  size_t length = strlen(path);
+  while (strncmp(path, "/", length) != 0 && path[length - 1] == '/')
+    --length;
+
+  return strndup(path, length);
+}
 
 int xc_db_open(xc_db_t **db, const char *path) {
 
@@ -24,14 +37,14 @@ int xc_db_open(xc_db_t **db, const char *path) {
   }
 
   // save the root for later file construction
-  d->root = strdup(path);
+  d->root = make_canon(path);
   if (ERROR(d->root == NULL)) {
     rc = ENOMEM;
     goto done;
   }
 
   // construct the directory if it does not already exist
-  if (mkdir(path, 0755) < 0) {
+  if (mkdir(d->root, 0755) < 0) {
     if (ERROR(errno != EEXIST)) {
       rc = errno;
       goto done;
