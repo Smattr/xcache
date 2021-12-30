0b6ad27e89d7c0da7a7059d849796995ac6f3b20
Matthew Fernandez <matthew.fernandez@gmail.com>
2021-12-30 12:04:39 -0800

    implement the rest of 'xc_db_dave'

--- libxcache/src/db_save.c
+++ libxcache/src/db_save.c
@@ -1,11 +1,15 @@
 #include "db.h"
 #include "debug.h"
 #include "fs_set.h"
+#include "pack.h"
 #include "path.h"
 #include "trace.h"
 #include <errno.h>
-#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <xcache/db.h>
+#include <xcache/hash.h>
 #include <xcache/proc.h>
 #include <xcache/trace.h>
 
@@ -33,6 +37,61 @@ int xc_db_save(xc_db_t *db, const xc_proc_t *proc,
     }
   }
 
-  // TODO
-  return ENOSYS;
+  char *output = NULL;
+  xc_trace_t *localised = NULL;
+  FILE *f = NULL;
+  int rc = -1;
+
+  // figure out where we will save this trace
+  xc_hash_t hash = 0;
+  rc = xc_hash_proc(&hash, proc);
+  if (ERROR(rc != 0))
+    goto done;
+  char hash_str[sizeof(hash) * 2 + 1];
+  snprintf(hash_str, sizeof(hash_str), "%" PRIxHASH, hash);
+  output = path_join(db->root, hash_str);
+  if (ERROR(output == NULL)) {
+    rc = ENOMEM;
+    goto done;
+  }
+
+  // localise the trace, so the database contents are relocatable
+  rc = trace_localise(db, &localised, observation);
+  if (ERROR(rc != 0))
+    goto done;
+
+  // save the entry itself
+  f = fopen(output, "w");
+  if (ERROR(f == NULL)) {
+    rc = errno;
+    goto done;
+  }
+  rc = pack_proc(f, proc);
+  if (ERROR(rc != 0))
+    goto done;
+  rc = pack_trace(f, localised);
+  if (ERROR(rc != 0))
+    goto done;
+  {
+    int r = fclose(f);
+    f = NULL;
+    if (ERROR(r == EOF)) {
+      rc = errno;
+      goto done;
+    }
+  }
+
+  // success
+  rc = 0;
+
+done:
+  if (f != NULL)
+    (void)fclose(f);
+  if (rc != 0 && output != NULL)
+    (void)unlink(output);
+  if (localised != NULL)
+    trace_deinit(localised);
+  free(output);
+
+  return rc;
 }
