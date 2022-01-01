b15bf4ce7c924bc6185ab14366df2757c709ab6b
Matthew Fernandez <matthew.fernandez@gmail.com>
2021-12-31 16:34:07 -0800

    remove legacy argv serialisation
    cb549bb7fb9893768d30e165680214858bab3fde implemented a better version of this.

--- libxcache/src/argv_serialise.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "macros.h"
-#include <stddef.h>
-
-/// serialise a list of arguments to a string, returning NULL on ENOMEM
-INTERNAL char *argv_serialise(size_t argc, char **argv);
