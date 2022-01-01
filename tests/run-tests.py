d5d833e069a2acf3f237e21098050a9f97ed6d19
Matthew Fernandez <matthew.fernandez@gmail.com>
2021-12-31 19:33:24 -0800

    port test suite to pytest
    Lets step into the 21st century.

--- tests/run-tests.py
+++ tests/run-tests.py
@@ -3,33 +3,27 @@
 from pathlib import Path
 import subprocess
 import tempfile
-import unittest
 
-class Tests(unittest.TestCase):
+def test_uncached_cat():
+  """
+  running cat with no prior cache should result in the correct output
+  """
+  with tempfile.TemporaryDirectory() as tmp:
 
-  def test_uncached_cat(self):
-    """
-    running cat with no prior cache should result in the correct output
-    """
-    with tempfile.TemporaryDirectory() as tmp:
+    # write some content to a test file
+    p = Path(tmp) / "test.txt"
+    with open(p, "wt") as f:
+      f.write("hello world\n")
 
-      # write some content to a test file
-      p = Path(tmp) / "test.txt"
-      with open(p, "wt") as f:
-        f.write("hello world\n")
+    # cat this file
+    output = subprocess.check_output(["xcache", "--", "cat", p],
+                                     universal_newlines=True)
+    assert output == "hello world\n"
 
-      # cat this file
-      output = subprocess.check_output(["xcache", "--", "cat", p],
-                                       universal_newlines=True)
-      self.assertEqual(output, "hello world\n");
-
-  def test_uncached_echo(self):
-    """
-    running echo with no prior cache should result in the correct output
-    """
-    output = subprocess.check_output(["xcache", "--", "my-echo", "hello",
-                                      "world"], universal_newlines=True)
-    self.assertEqual(output, "hello world\n");
-
-if __name__ == "__main__":
-  unittest.main()
+def test_uncached_echo():
+  """
+  running echo with no prior cache should result in the correct output
+  """
+  output = subprocess.check_output(["xcache", "--", "my-echo", "hello",
+                                    "world"], universal_newlines=True)
+  assert output == "hello world\n"
