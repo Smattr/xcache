1c435c3bf1a506513693360bbd41bf7ab706b3ed
Matthew Fernandez <matthew.fernandez@gmail.com>
2023-10-20 17:34:15 +1100

    hash read

---
+++ libxcache/src/hash_read.c
@@ -0,0 +1,24 @@
+#include "cbor.h"
+#include "hash_t.h"
+#include <assert.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+
+int hash_read(hash_t *hash, FILE *stream) {
+
+  assert(hash != NULL);
+  assert(stream != NULL);
+
+  *hash = (hash_t){0};
+  int rc = 0;
+
+  uint64_t data = 0;
+  if ((rc = cbor_read_u64(stream, &data)))
+    goto done;
+
+  hash->data = data;
+
+done:
+  return rc;
+}
