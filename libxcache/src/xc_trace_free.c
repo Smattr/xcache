d0050d95afb394e2b7094d6af24861cd1b03e1fd
Matthew Fernandez <matthew.fernandez@gmail.com>
2023-10-20 18:56:11 +1100

    trace_free

---
+++ libxcache/src/xc_trace_free.c
@@ -0,0 +1,20 @@
+#include "input_t.h"
+#include "trace_t.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <xcache/trace.h>
+
+void xc_trace_free(xc_trace_t *trace) {
+
+  if (trace == NULL)
+    return;
+
+  for (size_t i = 0; i < trace->n_inputs; ++i)
+    input_free(trace->inputs[i]);
+  free(trace->inputs);
+
+  if (trace->root)
+    (void)close(trace->root);
+
+  *trace = (xc_trace_t){0};
+}
