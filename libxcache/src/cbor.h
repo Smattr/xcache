a5190b3b4bd8ad76963a2d4c58befe1b0fdd2ad0
Matthew Fernandez <matthew.fernandez@gmail.com>
2023-10-20 17:28:11 +1100

    cbor read

---
+++ libxcache/src/cbor.h
@@ -0,0 +1,19 @@
+/// \file
+/// \brief serialisation and deserialisation
+///
+/// Some data structures need to be written to and then later read back from
+/// disk. The encoding for this is arbitrary, but we pick CBOR.
+
+#pragma once
+
+#include "../../common/compiler.h"
+#include <stdint.h>
+#include <stdio.h>
+
+/** read a ≤64-bit unsigned integer
+ *
+ * \param stream File to read from
+ * \param value [out] Result on success
+ * \return 0 on success or an errno on failure
+ */
+INTERNAL int cbor_read_u64(FILE *stream, uint64_t *value);
