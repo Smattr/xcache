c6cd078df6368dee6639ceb7c03e1324dee2e3bb
Matthew Fernandez <matthew.fernandez@gmail.com>
2023-10-21 10:45:25 +1100

    fix cbor tag conditional

--- libxcache/src/cbor_read_u64_raw.c
+++ libxcache/src/cbor_read_u64_raw.c
@@ -11,7 +11,7 @@ int cbor_read_u64_raw(FILE *stream, uint64_t *value, uint8_t bias) {
     return EIO;
   const uint8_t tag = (uint8_t)tag_int;
 
-  if (tag < 0x17 + bias) {
+  if (0x0 + bias <= tag && tag <= 0x17 + bias) {
     *value = tag;
     return 0;
   }
