aee0205fccc93e85b4bd4e6923251feb5cfe5b56
Matthew Fernandez <matthew.fernandez@gmail.com>
2023-10-22 21:46:18 +1100

    output_read

---
+++ libxcache/src/output_read.c
@@ -0,0 +1,51 @@
+#include "cbor.h"
+#include "output_t.h"
+#include <assert.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+
+int output_read(output_t *output, FILE *stream) {
+
+  assert(output != NULL);
+  assert(stream != NULL);
+
+  *output = (output_t){0};
+  output_t o = {0};
+  int rc = 0;
+
+  if ((rc = cbor_read_str(stream, &o.path)))
+    goto done;
+
+  {
+    uint64_t st_mode = 0;
+    if ((rc = cbor_read_u64(stream, &st_mode)))
+      goto done;
+    o.st_mode = (mode_t)st_mode;
+  }
+
+  {
+    uint64_t st_uid = 0;
+    if ((rc = cbor_read_u64(stream, &st_uid)))
+      goto done;
+    o.st_uid = (uid_t)st_uid;
+  }
+
+  {
+    uint64_t st_gid = 0;
+    if ((rc = cbor_read_u64(stream, &st_gid)))
+      goto done;
+    o.st_gid = (gid_t)st_gid;
+  }
+
+  if ((rc = cbor_read_str(stream, &o.cached_copy)))
+    goto done;
+
+  *output = o;
+  o = (output_t){0};
+
+done:
+  output_free(o);
+
+  return rc;
+}
--- libxcache/src/output_t.h
+++ libxcache/src/output_t.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "../../common/compiler.h"
+#include <stdio.h>
 #include <sys/stat.h>
 #include <xcache/trace.h>
 
@@ -12,6 +13,20 @@ typedef struct {
   char *cached_copy; ///< related path to cached content, if not directory
 } output_t;
 
+/** deserialise an output from a file
+ *
+ * \param output [out] Reconstructed output on success
+ * \param stream File to read from
+ * \return 0 on success or an errno on failure
+ */
+INTERNAL int output_read(output_t *output, FILE *stream);
+
+/** re-run the effects of an output
+ *
+ * \param output Output to replay
+ * \param owner Trace within which this output lives
+ * \return 0 on success or an errno on failure
+ */
 INTERNAL int output_replay(const output_t output, const xc_trace_t *owner);
 
 /** destroy an output
