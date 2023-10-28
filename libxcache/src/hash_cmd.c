9afba2888b207c2f8e54ea3f99a830aa15c63505
Matthew Fernandez <matthew.fernandez@gmail.com>
2023-10-28 14:18:08 +1100

    serialise commands to hash them

--- libxcache/src/hash_cmd.c
+++ libxcache/src/hash_cmd.c
@@ -1,8 +1,9 @@
+#include "cmd_t.h"
+#include "debug.h"
 #include "hash_t.h"
 #include <assert.h>
 #include <errno.h>
 #include <stdlib.h>
-#include <string.h>
 #include <xcache/cmd.h>
 
 int hash_cmd(const xc_cmd_t cmd, hash_t *hash) {
@@ -10,39 +11,35 @@ int hash_cmd(const xc_cmd_t cmd, hash_t *hash) {
   assert(hash != NULL);
 
   *hash = (hash_t){0};
-  unsigned char *buffer = NULL;
+  char *buffer = NULL;
+  size_t buffer_size = 0;
+  FILE *stream = NULL;
   int rc = 0;
 
-  // how many bytes do we need to concatenate everything with separating `'\0's?
-  size_t len = 0;
-  for (size_t i = 0; i < cmd.argc; ++i)
-    len += strlen(cmd.argv[i]) + 1;
-  len += strlen(cmd.cwd) + 1;
-
-  buffer = calloc(1, len);
-  if (buffer == NULL) {
-    rc = ENOMEM;
+  // create an in-memory stream
+  stream = open_memstream(&buffer, &buffer_size);
+  if (ERROR(stream == NULL)) {
+    rc = errno;
     goto done;
   }
 
-  // serialise everything into the buffer
-  {
-    size_t offset = 0;
-    for (size_t i = 0; i < cmd.argc; ++i) {
-      assert(offset < len && "miscalculated length");
-      const size_t size = strlen(cmd.argv[i]);
-      memcpy(&buffer[offset], cmd.argv[i], size + 1);
-      offset += size + 1;
-    }
-    assert(offset < len && "miscalculated length");
-    const size_t size = strlen(cmd.cwd);
-    memcpy(&buffer[offset], cmd.cwd, size + 1);
-  }
+  // serialise the command into this buffer
+  if (ERROR((rc = cmd_write(cmd, stream))))
+    goto done;
 
-  // compute the hash of this
-  *hash = hash_data(buffer, len);
+  // finalise the stream
+  if (ERROR(fclose(stream) < 0)) {
+    rc = errno;
+    goto done;
+  }
+  stream = NULL;
+
+  // hash the serialised representation
+  *hash = hash_data(buffer, buffer_size);
 
 done:
+  if (stream != NULL)
+    (void)fclose(stream);
   free(buffer);
 
   return rc;
