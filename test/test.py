ad1e59f7ee2c7bc7f422a1f3f78b4963d8102e56
Matthew Fernandez <matthew.fernandez@gmail.com>
2023-11-28 20:31:18 +1100

    strace before test

--- test/test.py
+++ test/test.py
@@ -2,11 +2,25 @@
 Xcache test suite
 """
 
+import os
 import subprocess
 import tempfile
 from pathlib import Path
+from typing import Union
 
 import pytest
+
+
+def strace(args: list[Union[Path | str]]):
+    """
+    `strace` a process, expecting it to succeed
+    """
+
+    # we need to disable LSan, which does not work under `strace`
+    env = os.environ.copy()
+    env["ASAN_OPTIONS"] = "detect_leaks=0"
+
+    subprocess.check_call(["strace", "-f", "--"] + args, env=env)
 
 
 @pytest.mark.parametrize("debug", (False, True))
@@ -48,6 +62,12 @@ def test_nop(debug: bool, record: bool, replay: bool, tmp_path: Path):
     """
     can we handle a no-op program?
     """
+    # First, `strace` the process we are about to test. If the test fails, the
+    # `strace` output will show what syscalls it made which may aid debugging.
+    # This is useful when, e.g., running on a new kernel where the dynamic
+    # loader or libc makes unanticipated syscalls.
+    strace(["nop"])
+
     args = ["xcache"]
     if debug:
         args += ["--debug"]
