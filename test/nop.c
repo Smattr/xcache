d396eb9fed72d9522275e0325e3c1874a5e64cf1
Matthew Fernandez <matthew.fernandez@gmail.com>
2023-11-28 20:25:47 +1100

    basic nop test

---
+++ test/nop.c
@@ -0,0 +1,9 @@
+/// \file
+///
+/// a program that does almost nothing
+///
+/// If we cannot record and replay this, we cannot do anything.
+
+int main(void) {
+  return 0;
+}
--- test/test.py
+++ test/test.py
@@ -42,6 +42,32 @@ def test_nonexistent(debug: bool, tmp_path: Path):
 
 
 @pytest.mark.parametrize("debug", (False, True))
+@pytest.mark.parametrize("record", (False, True))
+@pytest.mark.parametrize("replay", (False, True))
+def test_nop(debug: bool, record: bool, replay: bool, tmp_path: Path):
+    """
+    can we handle a no-op program?
+    """
+    args = ["xcache"]
+    if debug:
+        args += ["--debug"]
+    args += [f"--dir={tmp_path}/database"]
+    if record:
+        if replay:
+            args += ["--read-write"]
+        else:
+            args += ["--write-only"]
+    else:
+        if replay:
+            args += ["--read-only"]
+        else:
+            args += ["--disable"]
+    args += ["--", "nop"]
+
+    subprocess.check_call(args)
+
+
+@pytest.mark.parametrize("debug", (False, True))
 def test_version(debug: bool):
     """
     check --version does something reasonable
