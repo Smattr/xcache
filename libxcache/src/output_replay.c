34860efd343a13c2160061a43237118cc3c4e1b0
Matthew Fernandez <matthew.fernandez@gmail.com>
2023-12-10 10:46:40 +1100

    debug messages

--- libxcache/src/output_replay.c
+++ libxcache/src/output_replay.c
@@ -16,6 +16,7 @@ static int replay_chmod(const output_t output) {
 
   int rc = 0;
 
+  DEBUG("replaying chmod(\"%s\", %ld)", output.path, (long)output.chmod.mode);
   if (ERROR(chmod(output.path, output.chmod.mode) < 0)) {
     rc = errno;
     goto done;
@@ -31,6 +32,7 @@ static int replay_chown(const output_t output) {
 
   int rc = 0;
 
+  DEBUG("replaying chown(\"%s\", %ld, %ld)", output.path, (long)output.chown.uid, (long)output.chown.gid);
   if (ERROR(chown(output.path, output.chown.uid, output.chown.gid) < 0)) {
     rc = errno;
     goto done;
@@ -46,6 +48,7 @@ static int replay_mkdir(const output_t output) {
 
   int rc = 0;
 
+  DEBUG("replaying mkdir(\"%s\", %ld)", output.path, (long)output.mkdir.mode);
   if (ERROR(mkdir(output.path, output.mkdir.mode) < 0)) {
     // allow directory to already exist
     if (ERROR(errno != EEXIST)) {
@@ -75,6 +78,7 @@ static int replay_write(const output_t output, const xc_trace_t *owner) {
   int dst = -1;
   int rc = 0;
 
+  DEBUG("retrieving cached content from %s", output.write.cached_copy);
   src = openat(owner->root, output.write.cached_copy, O_RDONLY | O_CLOEXEC);
   if (ERROR(src < 0)) {
     rc = errno;
@@ -83,6 +87,7 @@ static int replay_write(const output_t output, const xc_trace_t *owner) {
 
   // we do not care so much about the mode because anything that was opened
   // `O_CREAT` will have a following `chmod` action recorded too
+  DEBUG("replaying open(\"%s\", …)", output.path);
   dst = open(output.path, O_WRONLY | O_CREAT | O_TRUNC | O_CLOEXEC, 0644);
   if (ERROR(dst < 0)) {
     rc = errno;
