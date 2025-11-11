0fc0c2550ac22561b516952e95ab7628e6821dae
Matthew Fernandez <matthew.fernandez@gmail.com>
2025-11-10 20:10:53 -0800

    support hashing of unmmappable files
    After this, the next barrier to caching an ASan-instrumented `nop` is `getpid`.

--- libxcache/src/hash_file.c
+++ libxcache/src/hash_file.c
@@ -4,6 +4,8 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <unistd.h>
@@ -14,8 +16,10 @@ int hash_file(const char *path, hash_t *hash) {
   assert(hash != NULL);
 
   int fd = -1;
+  FILE *f = NULL;
   void *base = MAP_FAILED;
   size_t size = 0;
+  char *buffer = NULL;
   int rc = 0;
 
   fd = open(path, O_RDONLY | O_CLOEXEC);
@@ -39,18 +43,35 @@ int hash_file(const char *path, hash_t *hash) {
   if (size > 0) {
     base = mmap(NULL, size, PROT_READ, MAP_PRIVATE, fd, 0);
     if (ERROR(base == MAP_FAILED)) {
-      // FIXME: handle hashing of unmappable files
-      rc = errno;
-      goto done;
+      // if we could not mmap the file, try reading it into memory as a fallback
+      f = fdopen(fd, "r");
+      if (ERROR(f == NULL)) {
+        rc = errno;
+        goto done;
+      }
+      fd = -1;
+      buffer = malloc(size);
+      if (ERROR(buffer == NULL)) {
+        rc = ENOMEM;
+        goto done;
+      }
+      if (ERROR(fread(buffer, size, 1, f) != 1)) {
+        rc = EIO;
+        goto done;
+      }
+      *hash = hash_data(buffer, size);
+    } else {
+      *hash = hash_data(base, size);
     }
   } else {
-    base = NULL;
+    *hash = hash_data(NULL, 0);
   }
 
-  *hash = hash_data(base, size);
-
 done:
-  if (base != MAP_FAILED && base != NULL)
+  free(buffer);
+  if (f != NULL)
+    (void)fclose(f);
+  if (base != MAP_FAILED)
     (void)munmap(base, size);
   if (fd >= 0)
     (void)close(fd);
