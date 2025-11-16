f47d8579f4d923ec843c4a89fb9db04498ae38cc
Matthew Fernandez <matthew.fernandez@gmail.com>
2025-11-16 14:03:59 -0800

    fix some missing syscall guards

--- libxcache/src/sysenter.c
+++ libxcache/src/sysenter.c
@@ -77,13 +77,19 @@ int sysenter(inferior_t *inf, thread_t *thread) {
 #define SYSEXIT_IGNORE(call) // nothing
 #include "ignore.h"
 
+#ifdef __NR_clone
   DO(clone);
+#endif
+#ifdef __NR_clone3
   DO(clone3);
+#endif
 
   // `execve` is one of the few syscalls we must handle on enter because the
   // caller’s address space does not exist at exit, making it impossible for us
   // to peek its arguments
+#ifdef __NR_execve
   DO(execve);
+#endif
 
   rc = ENOTSUP;
 done:
