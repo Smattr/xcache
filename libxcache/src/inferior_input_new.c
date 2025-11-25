5107a22f8a7dd9de6533d8c48edb6c5852f46a43
Matthew Fernandez <matthew.fernandez@gmail.com>
2025-11-24 19:19:26 -0800

    get more precise about when we are allowed to elide reads

--- libxcache/src/inferior_input_new.c
+++ libxcache/src/inferior_input_new.c
@@ -9,13 +9,21 @@ int inferior_input_new(inferior_t *inf, const input_t input) {
 
   assert(inf != NULL);
 
-  // If this is already considered an output, ignore it. This can happen when
-  // e.g. the target writes out a temporary file and then reads it back in.
-  for (size_t i = 0; i < LIST_SIZE(&inf->outputs); ++i) {
-    if (strcmp(LIST_AT(&inf->outputs, i)->path, input.path) == 0) {
-      DEBUG("skipping \"%s\" as input because it is already an output",
-            input.path);
-      return 0;
+  // A prior file write “dominates” a file read, in the sense that the read is
+  // only consuming data the tracee already had, not new external input.This can
+  // happen when e.g. the target writes out a temporary file and then reads it
+  // back in.
+  if (input.tag == INP_READ) {
+    for (size_t i = 0; i < LIST_SIZE(&inf->outputs); ++i) {
+      const output_t *const output = LIST_AT(&inf->outputs, i);
+      if (output->tag != OUT_WRITE)
+        continue;
+      if (strcmp(output->path, input.path) == 0) {
+        DEBUG("skipping read of \"%s\" as input because a write of it is "
+              "already an output",
+              input.path);
+        return 0;
+      }
     }
   }
 
