8310f40b76aae2ce5329c6970726ae52a8b4b980
Matthew Fernandez <matthew.fernandez@gmail.com>
2025-11-28 17:05:57 -0800

    de-dupe chmods

--- libxcache/src/inferior_output_new.c
+++ libxcache/src/inferior_output_new.c
@@ -3,13 +3,43 @@
 #include "list.h"
 #include "output_t.h"
 #include <assert.h>
+#include <stdbool.h>
 #include <stddef.h>
+#include <string.h>
+
+static bool is_dominated_by(const output_t sub, const output_t dom) {
+
+  // `chmod;chmod` can be de-duped into just `chmod`
+  if (sub.tag == OUT_CHMOD && dom.tag == OUT_CHMOD) {
+    assert(sub.chmod.mode == 0 && "chmod output’s mode not set to placeholder");
+    assert(dom.chmod.mode == 0 && "chmod output’s mode not set to placeholder");
+    if (strcmp(sub.path, dom.path) == 0) {
+      DEBUG("dropping chmod of \"%s\" as output because a later chmod of it "
+            "was seen",
+            sub.path);
+      return true;
+    }
+  }
+
+  return false;
+}
 
 int inferior_output_new(inferior_t *inf, const output_t output) {
 
   assert(inf != NULL);
 
   int rc = 0;
+
+  // we can drop any earlier output that is dominated by this output
+  for (size_t i = 0; i < LIST_SIZE(&inf->outputs);) {
+    output_t prior = *LIST_AT(&inf->outputs, i);
+    if (is_dominated_by(prior, output)) {
+      (void)LIST_POP(&inf->outputs, i);
+      output_free(prior);
+    } else {
+      ++i;
+    }
+  }
 
   if (ERROR((rc = LIST_PUSH_BACK(&inf->outputs, output))))
     goto done;
