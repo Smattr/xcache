183ab990255b197d7befb80e222562c0e9159af6
Matthew Fernandez <matthew.fernandez@gmail.com>
2025-12-05 18:26:03 -0800

    Revert "functionality for locating libasan"
    This reverts commit 110562885344d588f8bf70c3010ddb740208ac01.

--- libxcache/src/find_me.h
+++ libxcache/src/find_me.h
@@ -19,13 +19,3 @@ INTERNAL int find_lib(char **me);
 /// @param spy [out] Path to libxcache-spy.so on success
 /// @return 0 on success or an errno on failure
 INTERNAL int find_spy(char **spy);
-
-/// find an absolute path to libasan
-///
-/// This only finds libasan when the containing process has loaded libasan. It
-/// is not capable of finding a libasan.so on disk that is not being used by the
-/// containing process.
-///
-/// @param asan [out] Absolute path to libasan on success
-/// @return 0 on success or an errno on failure
-INTERNAL int find_asan(char **asan);
